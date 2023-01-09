8a4015ba32ad7db274a7ef20089466ad45e9a0f3
JeneryBautistaCanul <jenerybautistacanul@gmail.com>
2023-01-08 22:36:14 -0600

    feat(Basic Concepts): Add the Basic Concepts folder.
    The folder contains lessons and practices to understand the Basic Concepts topic, and .py files with the answers.

---
+++ Basic Concepts/3.1 Lesson.py
@@ -0,0 +1,24 @@
+#3.1 Lesson
+#Simple Operations
+#Answers
+
+#Addition and Subtraction
+print(2 + 2)
+print(5 + 4 - 3)
+
+#Practice
+#What does this code output?
+print(1 + 2 + 3)
+6
+
+#Division
+print(10 / 2)
+
+#Multiple operations
+#Multiplication and addition
+print(2 * (3 + 4))
+
+#Practice
+#Which option is the output of this code? 
+print((4 + 8) / 2)
+6.0
