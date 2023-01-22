0c065e79e1077b904b87c15fc153c7e2beb8c6c8
JeneryBautistaCanul <jenerybautistacanul@gmail.com>
2023-01-21 22:37:37 -0600

    feat(Strings): Add the Strings folder.
    The folder contains lessons and practices to understand the Strings and .py files with the answers.

---
+++ Strings/10.2 Practice.py
@@ -0,0 +1,13 @@
+#10.2 Practice
+#Reach For The Starts
+#Answer
+
+#This code tries to make a triangle out of stars. But oh no! There’s an error in there somewhere-it outputs all the stars on one line, instead of separate lines.
+
+#Fix the code to output a triangle of stars that has 4 rows.
+#> You can use **\n** newlines to create line breaks in the string.  
+#**Important**: Remove any extra spaces to match the required output.
+
+#Your code goes here
+#triangle out of stars
+print("*\n**\n***\n****") #New line
---
+++ Strings/12.1 Lesson.py
@@ -0,0 +1,18 @@
+#12.1 Lessons
+#Module 2 quiz
+
+#Which of the following is a string?
+'42'
+
+#Fill in the blanks to output "Welcome".
+print("Welcome")
+
+#What’s the output of this code?
+print("42" + '5')
+425
+
+#Fill in the blanks to output "ABC":
+print("A" + "B" + "C")
+
+#Which of the following is the escape character?
+#\
