d3d1dc33d2d2ecc88702ecbf80884544e49e58e4
JeneryBautistaCanul <jenerybautistacanul@gmail.com>
2023-01-24 14:40:46 -0600

    Add files via upload

---
+++ Strings/10.1 Lesson.py
@@ -0,0 +1,24 @@
+#10.1 Lesson
+#Newlines
+#Answers
+
+#Create a line break
+#The line break goes before the word
+print('One \nTwo \nThree')
+
+#Practice
+#Which og the following options results in exactly two lines?
+print("Hello \n world")
+
+#Create line break multiples
+print("""this
+is a  multine
+text""")
+
+#Practice
+#How many lines will the following code output?
+print("Hi")
+print("""This 
+is
+great""")
+4
---
+++ Strings/11.1 Lesson.py
@@ -0,0 +1,22 @@
+#11.1 Lesson
+#String Operations
+#Answers
+
+#Concatenation
+print("Spam" + 'eggs')
+
+#String of numbers
+print("2" + "2")
+
+#Practice
+#Fill in the blanks to produce “Python rocks”.
+print("Python" + "rocks")
+
+#Sring operations
+print("spam" * 3)
+print(4 * '2')
+
+#Practice
+#What is output of this code?
+print(3 * '7')
+777
